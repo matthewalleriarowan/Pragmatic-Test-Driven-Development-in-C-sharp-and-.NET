d14a7da7db41ac2b6cfd6fed0b0bbbbc138a845e
Matthew Rowan <matthewarowan@gmail.com>
2023-12-02 20:40:50 -0800

    now TestRunner doesn't throw exception

--- Uqs.Weather.TestRunner/Program.cs
+++ Uqs.Weather.TestRunner/Program.cs
@@ -9,4 +9,4 @@ double f1 = controller.ConvertCToF(-1.0);
 if (f1 != 30.20d) throw new Exception("Invalid");
 double f2 = controller.ConvertCToF(1.2);
 if (f2 != 34.16d) throw new Exception("Invalid");
-throw new Exception("Tests Passed!");
+Console.WriteLine("Tests Passed!");
