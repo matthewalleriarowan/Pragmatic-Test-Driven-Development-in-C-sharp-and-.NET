23cc08063a22f5027a460a1304618abc64eda052
Matthew Rowan <matthewarowan@gmail.com>
2023-12-05 14:50:16 -0800

    updated DI class ClientStub to produce reliably testable output

--- Uqs.Weather/ClientStub.cs
+++ Uqs.Weather/ClientStub.cs
@@ -9,15 +9,20 @@ namespace Uqs.Weather
             const int DAYS = 7;
             WeatherResponse res = new WeatherResponse();
             res.List = new Forecast[DAYS];
-            DateTime now = DateTime.Now;
-            for(int i = 0; i < DAYS; i++)
+            DateTime now = DateTime.Now.Date;
+
+            System.Diagnostics.Debug.WriteLine(now.ToString());
+
+            for (int i = 0; i < DAYS; i++)
             {
                 res.List[i] = new Forecast();
                 res.List[i].Dt = now.AddDays(i);
+                res.List[i].Dt = now.AddHours(12);
                 res.List[i].Main = new Main
                 {
                     Temp = Random.Shared.Next(-20, 55)
                 };
+                System.Diagnostics.Debug.WriteLine(res.List[i].Dt.ToString());
             }
             return Task.FromResult(res);
         }
